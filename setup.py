507837b476c721efb8da10b9230e0e84d82eb54e
Rob Brackett <rob@robbrackett.com>
2026-04-29 13:13:03 -0700

    Upgrade to current Sphinx and Numpydoc
    Everything still basically works the same, at least as far as the issues demonstrated here.

--- setup.py
+++ setup.py
@@ -6,11 +6,11 @@ setup(
     version='1.0.0',
     description="Example of issues with numpydoc attributes and properties",
     author="Rob Brackett",
-    python_requires='>=3.6.0',
+    python_requires='>=3.11.0',
     packages=find_packages(exclude=['docs']),
     install_requires=[
-        'sphinx ~=3.2.1',
-        'numpydoc ~=1.1.0'
+        'sphinx ~=9.1.0',
+        'numpydoc ~=1.10.0'
     ],
     license="BSD (3-clause)",
 )
